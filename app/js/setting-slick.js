d3722b62ed12e95f68b01b0f5ab62d1cc1143430
Gladibeth <gladibethdelos@gmail>
2019-08-29 14:34:37 -0400

    Add mask banner and slider clients

--- app/js/setting-slick.js
+++ app/js/setting-slick.js
@@ -3,8 +3,8 @@ $('.main-banner__content').slick({
   autoplay: false,
   slidesToShow: 1,
   slidesToScroll: 1,
-  dots: true,
-  arrows: false,
+  dots: false,
+  arrows: true,
   responsive: [{
       breakpoint: 1200,
       settings: {
@@ -29,6 +29,41 @@ $('.main-banner__content').slick({
         dots: false,
         autoplay: true,
         arrows: false,
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
+
+$('.main-client__content').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 4,
+  dots: true,
+  arrows: true,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
         autoplaySpeed: 1000
       }
     }
